2cb93f347e51414b4161818caa83a388b74c02a3
viksiik <viksiik.k@gmail.com>
2025-02-12 19:01:01 +0200

    Added LAB1

--- LAB1/lib/main.dart
+++ LAB1/lib/main.dart
@@ -1,125 +1,221 @@
 import 'package:flutter/material.dart';
 
+// Написати програму під платформу Андроїд, яка має інтерфейс для введення
+// або/та вибору даних згідно варіанту (таблиця) і відображає результат
+// взаємодії з цим
+// інтерфейсом у деяке текстове поле цього інтерфейсу. Передбачити наступне:
+// якщо не всі дані введені або обрані, а користувач натискає кнопку для
+// отримання результату,
+// то відобразити вікно, що спливає, з повідомленням завершити введення
+// всіх даних.
+
+// Вікно містить групу прапорів (площа і периметр), тобто чек-бокси, групу
+// опцій (різні фігури), тобто радіо-батони, та кнопку «ОК». Вивести
+// інформацію щодо вибору при натисканні на кнопку «ОК» у деяке текстове
+// поле.
+
+
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    home: ShapeCalculator(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ShapeCalculator extends StatefulWidget {
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const ShapeCalculator({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ShapeCalculatorState createState() => _ShapeCalculatorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ShapeCalculatorState extends State<ShapeCalculator> {
+  String? selectedShape;
+  bool isAreaChecked = false;
+  bool isPerimeterChecked = false;
+  String result = "";
+  String selectedFont = 'Montserrat';
 
-  void _incrementCounter() {
+  void calculate() {
+    if (selectedShape == null || (!isAreaChecked && !isPerimeterChecked)) {
+      _showAlert();
+      return;
+    }
+
+    String output = "Your chosen shape: $selectedShape\n";
+    if (isAreaChecked) output += "- Need to calculate area\n";
+    if (isPerimeterChecked) output += "- Need to calculate perimeter\n";
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      result = output;
     });
   }
 
+  void _showAlert() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Error",
+            style: TextStyle(
+              fontFamily: selectedFont,
+              fontSize: 20,
+              color: Colors.redAccent,
+              fontWeight: FontWeight.bold,)),
+        content: Text("Choose shape and at least one parameter to continue.",
+            style: TextStyle(
+              fontFamily: selectedFont,
+              fontSize: 16,
+              fontWeight: FontWeight.w400,)),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text("OK",
+                style: TextStyle(
+                  fontFamily: selectedFont,
+                  fontSize: 16,
+                  color: Colors.blueGrey,
+                  fontWeight: FontWeight.bold,)
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(64),
+        child: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFFE48AFF), Color(0xFF97E9FF)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            borderRadius:
+              BorderRadius.vertical(bottom: Radius.circular(20)),
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xFFC5C5C5),
+                blurRadius: 10,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
+          child: AppBar(
+            title: Row(
+              children: [
+                const Icon(Icons.calculate, color: Colors.white),
+                const SizedBox(width: 10),
+                Text("Objects calculator",
+                    style: TextStyle(
+                      fontFamily: selectedFont,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,)
+                ),
+              ],
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Choose parameters:",
+                style: TextStyle(
+                    fontFamily: selectedFont,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600)
+            ),
+
+            CheckboxListTile(
+              title: Text("Area",
+                  style: TextStyle(
+                      fontFamily: selectedFont,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400)
+              ),
+              value: isAreaChecked,
+              onChanged: (value) {
+                setState(() {
+                  isAreaChecked = value!;
+                });
+              },
+            ),
+
+            CheckboxListTile(
+              title: Text("Perimeter",
+                  style: TextStyle(
+                      fontFamily: selectedFont,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400)),
+              value: isPerimeterChecked,
+              onChanged: (value) {
+                setState(() {
+                  isPerimeterChecked = value!;
+                });
+              },
+            ),
+
+            const SizedBox(height: 24),
+
+            Text("Choose shape:",
+                style: TextStyle(
+                    fontFamily: selectedFont,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600)
+            ),
+
+            Column(
+              children: ["Circle", "Square", "Triangle", "Rectangle"].map((shape) {
+                return RadioListTile(
+                  title: Text(shape,
+                      style: TextStyle(
+                          fontFamily: selectedFont,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400)
+                  ),
+                  value: shape,
+                  groupValue: selectedShape,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedShape = value as String?;
+                    });
+                  },
+                );
+              }).toList(),
+            ),
+
+            const SizedBox(height: 24),
+
+            Center(
+              child: ElevatedButton(
+                onPressed: calculate,
+                child: Text("OK",
+                    style: TextStyle(
+                        fontFamily: selectedFont,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600)),
+              ),
+            ),
+
+            const SizedBox(height: 24),
+
+            Text(result,
+                style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w400)
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
